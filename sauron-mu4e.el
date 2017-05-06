3062256eb543577f5ee96d33f8f3239d8ed8968a
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-06 09:20:55 +0200

    Starting package documentation

--- sauron-mu4e.el
+++ sauron-mu4e.el
@@ -1,3 +1,38 @@
+;;; sauron-mu4e.el --- Sauron integration of mu4e
+
+;; Copyright (C) 2017  Sébastien Le Maguer
+
+;; Author: Sébastien Le Maguer <slemaguer@coli.uni-saarland.de>
+;; Keywords: buffer, convenience, comm
+;; Package-Requires: ((cl-lib "0.2"))
+;; X-URL: https://github.com/seblemaguer/sauron-mu4e
+;; URL: https://github.com/seblemaguer/sauron-mu4e
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provides mu4e  activity tracking for sauron
+
+;;; Installation:
+
+;; Add this to your .emacs:
+
+;; (add-to-list 'load-path "/folder/containing/file")
+;; (require 'sauron-mu4e)
+;; (sauron-mu4e-start)
 
 
 ;;; Code:
